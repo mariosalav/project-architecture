177d9fcb2af944ab2d311fb93938663d16675334
Mario Salazar <jsalazar@ext.laligatech.com>
2023-08-14 15:00:16 -0500

    chore: initial project configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,20 @@ import { defineConfig } from 'vite';
 import path from 'node:path';
 import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react';
-import { name } from './package.json';
+import { name, peerDependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      entry: path.resolve(__dirname, 'lib/index.ts'),
       name,
       fileName: (format) => `${name}.${format}.ts`,
       formats: ['es'],
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
     },
   },
 });
