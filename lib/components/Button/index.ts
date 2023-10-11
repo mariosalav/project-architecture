622f50332e6457abed2f75f43ebf522f84cf7134
Mario Salazar <jsalazar@ext.laligatech.com>
2023-10-11 11:44:06 -0500

    chore(button.tsx): export iconprop type

--- lib/components/Button/index.ts
+++ lib/components/Button/index.ts
@@ -1,2 +1,3 @@
 export { default } from './Button';
 export { type ButtonProps, ButtonType } from './Button.model';
+export { type IconProp } from '@fortawesome/fontawesome-svg-core';
--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button, ButtonType, type ButtonProps } from './Button';
+export { type IconProp } from '@fortawesome/fontawesome-svg-core';
 export { default as Input } from './Input';
