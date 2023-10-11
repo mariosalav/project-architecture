e26192e0b6e2023f2bfc81d5eebdc00802eca0ac
Mario Salazar <jsalazar@ext.laligatech.com>
2023-10-11 11:58:46 -0500

    chore(package.json): modified dependencies, revert export from button component

--- lib/components/Button/index.ts
+++ lib/components/Button/index.ts
@@ -1,3 +1,2 @@
 export { default } from './Button';
 export { type ButtonProps, ButtonType } from './Button.model';
-export { type IconProp } from '@fortawesome/fontawesome-svg-core';
--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,3 +1,2 @@
 export { default as Button, ButtonType, type ButtonProps } from './Button';
-export { type IconProp } from '@fortawesome/fontawesome-svg-core';
 export { default as Input } from './Input';
